8067c8427fe21abaca72c97e0b80fc4db11fd4d7
balamurugan <balarvs2002@gmail.com>
2016-02-19 00:27:23 +0530

    added comments

--- SampleDemo/Program.cs
+++ SampleDemo/Program.cs
@@ -8,6 +8,10 @@ namespace SampleDemo
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
